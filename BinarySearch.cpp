b22aee6c6808fc352777b8ea7f1d55aed6cfe05c
gseptarisa-ctrl <gseptarisa@gmail.com>
2026-04-16 17:58:05 +0700

    pengecekan input

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -4,3 +4,18 @@ using namespace std;
 int element[10];
 int nPanjang;
 int x;
+
+void input(){
+  while (true){
+    cout << "masukkan banyak nya elemen pada array (makssimal 10) : ";
+    cin >> nPanjang;
+
+    if (nPanjang <= 10)
+    {
+      break;
+    }
+    else
+    {
+      cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10. Silahkan coba lagi.\n";
+    }
+  }
