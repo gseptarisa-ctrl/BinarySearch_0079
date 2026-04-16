335f5a25d19c24f6c107b440800614955472ba54
gseptarisa-ctrl <gseptarisa@gmail.com>
2026-04-16 18:00:14 +0700

    proses pengurutan data menggunakan bubble sort

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -30,3 +30,21 @@ for (int i = 0; i < nPanjang; i++)
   cin >> element[i];
   }
 }
+
+void bubbleSortArray()
+{
+    int pass = 1;
+    do
+    {
+        for (int j = 0; j <= nPanjang - 1 - pass; j++)
+        {
+          if (element[j] > element[j+1])
+          {
+            int temp = element[j];
+            element[j] = element[j + 1];
+            element[j + 1] = temp;
+          }
+        }
+        pass++;
+    } while (pass <= nPanjang - 1);
+}
