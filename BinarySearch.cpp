79946d19b66c1c393c118d7b5ebda5e3be8dbbeb
gseptarisa-ctrl <gseptarisa@gmail.com>
2026-04-16 17:59:07 +0700

    langkah mengisi data ke dalam array

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -19,3 +19,14 @@ void input(){
       cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10. Silahkan coba lagi.\n";
     }
   }
+
+cout << "\n=======================================\n";
+cout << "           masukkan elemen array          \n";
+cout << "\n=======================================\n";
+
+for (int i = 0; i < nPanjang; i++)
+  {
+  cout << "data ke-" << (i+1) << " = ";
+  cin >> element[i];
+  }
+}
